2e90202e6ccae8e18dfa6a30798f13f9e3ca07a4
kimi <kay351601@aliyun.com>
2019-10-15 19:34:34 +0800

    just test

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -131,10 +131,10 @@ public:
         nPruneAfterHeight = 100000;
 
 //        genesis = CreateGenesisBlock(1231006505, 2083236893, 0x1d00ffff, 1, 50 * COIN);
-        genesis = CreateGenesisBlock(1231006505, 300, 0x1d00ff00, 1, 50 * COIN);
+        genesis = CreateGenesisBlock(1231006505, 584, 0x1d00ff00, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
 //        assert(consensus.hashGenesisBlock == uint256S("0x000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"));
-        assert(consensus.hashGenesisBlock == uint256S("00af9934889e31b8fb2ce57dfd2ab3249b5707fb3531d978d5ecaf2c2af199bf"));
+        assert(consensus.hashGenesisBlock == uint256S("001dcedd8f3f1fd3f3747ccc11802ab0bb7535776bc87f04d7d2227b979646f2"));
         assert(genesis.hashMerkleRoot == uint256S("0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"));
 
         // Note that of those which support the service bits prefix, most only support a subset of
